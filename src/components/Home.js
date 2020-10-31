ba920a19a93d365af04fb52874beb84b6723c730
Nikhil Khandelwal <33507803+Niksgupta@users.noreply.github.com>
2020-10-31 12:05:40 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,14 +69,14 @@ function HomeFun() {
         <Card.Text>Cases {data.cases}</Card.Text>
         <Card.Text>Deaths {data.deaths}</Card.Text>
         <Card.Text>Recovered {data.recovered}</Card.Text>
-        <Card.Text>Today's cases {data.todatCases}</Card.Text>
+        <Card.Text>Today's cases {data.todayCases}</Card.Text>
         <Card.Text>Today's deaths {data.todayDeaths}</Card.Text>
         <Card.Text>Active {data.active}</Card.Text>
         <Card.Text>Critical {data.critical}</Card.Text>
       </Card.Body>
       
     </Card>
-    );
+    )
 
   });
   // columns settings 
